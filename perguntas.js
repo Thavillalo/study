fdb56fb10f1a7a50455f4ae0744030c383470dc4
Thavillalo <00001129508079sp@al.educacao.sp.gov.br>
2024-11-26 13:45:16 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,39 @@
+criaCartao(
+    'História',
+    'Qual evento marcou o início da Idade Média?',
+    'A queda do Império Romano do Ocidente, em 476 d.C.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior oceano do mundo?',
+    'Oceano Pacífico.'
+)
+
+criaCartao(
+    'Ciências',
+    'Qual é o elemento químico representado pelo símbolo O na tabela periódica?',
+    ' Oxigênio.'
+)
+
+criaCartao(
+    'Literatura ',
+    'Quem escreveu o livro Dom Quixote?',
+    'Miguel de Cervantes.'
+)
+
+criaCartao(
+    'Matemática ',
+    'Qual é o resultado da soma de 3/4 e 1/2?',
+    '5/4 ou 1,25'
+)
+
+criaCartao(
+    'Cinema',
+    'Qual filme ganhou o Oscar de Melhor Filme em 1994?',
+    'Forrest Gump.'
+)
+
+
+
+
